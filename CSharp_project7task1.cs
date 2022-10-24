397493e469183694b8450d059e40e95c488f0b1e
Nikita Kudashov <113311079+NikitaKudashov14@users.noreply.github.com>
2022-10-24 15:59:00 +0300

    Update CSharp_project7task1.cs

--- CSharp_project7task1.cs
+++ CSharp_project7task1.cs
@@ -9,39 +9,71 @@ namespace lesson
     {
         public string Surname;
         public string Speciality;
-        public int Form;
+        public int form = 0;
+        public string path1 = $"1.docx";
+        public int Form {
+            set {
+                Console.WriteLine("Процесс изменения данных");
+                if (value < 0 || value > 6) {
+                    Console.WriteLine("Ошибка");
+                } else {
+                    form = value; 
+                }
+            }
+            
+            get {
+                Console.WriteLine("Процесс передачи данных");
+                return form;
+            }
+        }
         
         public Student() { }
-        public Student(string surname, string speciality, int form)
+        public Student(string surname, string speciality)
         {
             Surname = surname;
             Speciality = speciality;
             Form = form;
+            
         }
+        
 
         public void say_info()
         {
             Console.WriteLine($"Surname: {Surname} | Speciality:{Speciality} | Form:{Form}");
         }
-
-        public void update_info()
-        {
-
+        
+        public void delete_data() {
+            File.WriteAllText(path1, "");
         }
+        
+        
     }
     class Program
     {
         static void Main(string[] args)
         {
             Student Kudashov = new Student();
-            Student Marinina = new Student("Marinina", "Information security", 1);
+            Student Marinina = new Student("Marinina", "Information security");
+            Marinina.Form = 1;
             Kudashov.say_info();
             Marinina.say_info();
+            Console.WriteLine(Marinina.Form);
+            string path = $"1.docx";
             string info = $"Surname: {Marinina.Surname} Speciality: {Marinina.Speciality} Form:{Marinina.Form}";
-
-            string path = @"C: \Users\Никита\OneDrive\Рабочий стол\1.txt";
-            File.WriteAllText(path, info);
+            
+            string user_input = Console.ReadLine();
+            if (user_input == "Save Data") {
+                File.WriteAllText(path, info);
+            } else if (user_input == "Delete Data") {
+                Marinina.delete_data();
+            }
+            
+        
+            
+            
+            
         }
 
     } 
+     
 }
