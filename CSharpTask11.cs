222c806d796095fa511365f60908a9d711a50482
Nikita Kudashov <113311079+NikitaKudashov14@users.noreply.github.com>
2022-12-03 21:25:50 +0300

    Update CSharpTask11.cs

--- CSharpTask11.cs
+++ CSharpTask11.cs
@@ -4,131 +4,181 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
-abstract class Transport {
-    public abstract string Name {get; set;}
-    public abstract int Speed {get; set;}
-    
+abstract class Transport
+{
+    public abstract string Name { get; set; }
+    public abstract int Speed { get; set; }
+
 }
 
-class Car : Transport {
+class Car : Transport
+{
     int speed;
     string name;
-    public override string Name {
-        get {
+    public override string Name
+    {
+        get
+        {
             return name;
         }
-        set {
+        set
+        {
             name = value;
         }
     }
-    
-    public override int Speed {
-        get {
+
+    public override int Speed
+    {
+        get
+        {
             return speed;
         }
-        set {
+        set
+        {
             speed = value;
         }
     }
 }
 
-class Train : Transport {
+class Train : Transport
+{
     int speed;
     string name;
-    public override string Name {
-        get {
+    public override string Name
+    {
+        get
+        {
             return name;
         }
-        set {
+        set
+        {
             name = value;
         }
     }
-    
-    public override int Speed {
-        get {
+
+    public override int Speed
+    {
+        get
+        {
             return speed;
         }
-        set {
+        set
+        {
             speed = value;
         }
     }
 }
 
-class Airplane : Transport {
+class Airplane : Transport
+{
     int speed;
     string name;
-    public override string Name {
-        get {
+    public override string Name
+    {
+        get
+        {
             return name;
         }
-        set {
+        set
+        {
             name = value;
         }
     }
-    
-    public override int Speed {
-        get {
+
+    public override int Speed
+    {
+        get
+        {
             return speed;
         }
-        set {
+        set
+        {
             speed = value;
         }
     }
 }
 
-class TransportCompany {
-    List <Transport> list = new List <Transport>(); 
-    public void AddElement(Transport t) {
+class TransportCompany
+{
+    List<Transport> list = new List<Transport>();
+    public void AddElement(Transport t)
+    {
         list.Add(t);
     }
-    
-    public void RemoveElement(int t){
+
+    public void RemoveElement(int t)
+    {
         list.RemoveAt(t);
     }
-    
-    public void UpdateElement(Transport t, int index) {
-        
-                list[index] = t;
-            
+
+    public void UpdateElement(Transport t, int index)
+    {
+
+        list[index] = t;
+
     }
-    
-    public void Data() {
-        foreach (var item in list) {
+
+    public void Data()
+    {
+        foreach (var item in list)
+        {
             Console.WriteLine(item);
         }
-        
-        if (list.Count == 0) {
+
+        if (list.Count == 0)
+        {
             Console.WriteLine("Ошибка!");
         }
     }
-    
+
 }
-class HelloWorld {
-  static void Main() {
+class HelloWorld
+{
+    static void Main()
+    {
         int input = 0;
+        TransportCompany t1 = new TransportCompany();
         Console.WriteLine("Выберите действие. 1 - добавить элемент. 2 - удалить элемент. 3 - изменить элемент. 4 - вывести информацию. 5 - выход из программы");
-        while (input != 5) {
+        while (input != 5)
+        {
             input = Convert.ToInt32(Console.ReadLine());
-            if (input == 1) {
-                TransportCompany t1 = new TransportCompany();
-                t1.AddElement(new Airplane());
-            } else if (input == 2) {
-                TransportCompany t1 = new TransportCompany();
-                t1.RemoveElement(0);
-            } else if (input == 3) {
-                TransportCompany t1 = new TransportCompany();
+            if (input == 1)
+            {
+                Console.WriteLine("Что именно нужно добавить? Машину, самолет или поезд?");
+                string choice = Console.ReadLine();
+                if (choice == "Машину")
+                {
+                    t1.AddElement(new Car());
+                } else if (choice == "Самолет")
+                {
+                    t1.AddElement(new Airplane());
+                } else if (choice == "Поезд")
+                {
+                    t1.AddElement(new Train());
+                }
+            }
+            else if (input == 2)
+            {
+                Console.WriteLine("Выберите индекс элемента, который нужно удалить");
+                int position = Convert.ToInt32(Console.ReadLine());
+                t1.RemoveElement(position);
+            }
+            else if (input == 3)
+            {
                 Transport Boeing = new Airplane();
                 t1.UpdateElement(Boeing, 0);
             }
-            else if (input == 4) {
-                TransportCompany t1 = new TransportCompany();
+            else if (input == 4)
+            {
                 t1.Data();
-            } else if (input == 5) {
+            }
+            else if (input == 5)
+            {
                 break;
             }
-            
-            
+
+
         }
-        
-  }
+
+    }
 }
